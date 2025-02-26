b809f568d58eea6839cda0712329d8884a33b51d
nguyencuongzth <nguyencuongzth@gmail.com>
2025-02-26 09:07:16 +0700

    change host

--- src/models/db.ts
+++ src/models/db.ts
@@ -3,7 +3,8 @@ import mysql from 'mysql2/promise';
 const pool = mysql.createPool({
     user: 'just.ngcuong',
     password: 'JNC@123',
-    host: 'localhost',
+    // host: 'localhost',
+    host: ' 14.238.29.226',
     port: 3306,
     database: 'souldb',
     waitForConnections: true,

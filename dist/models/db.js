27b599d1ed285debd6b5432abfb4593e4a80c82e
nguyencuongzth <nguyencuongzth@gmail.com>
2025-02-26 09:29:21 +0700

    asd

--- dist/models/db.js
+++ dist/models/db.js
@@ -4,14 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const promise_1 = __importDefault(require("mysql2/promise"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const pool = promise_1.default.createPool({
-    user: 'just.ngcuong',
-    password: 'JNC@123',
-    host: 'localhost',
-    port: 3306,
-    database: 'souldb',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT) || 3306,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
 });
 exports.default = pool;
